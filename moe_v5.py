9b38e7a827735b7416ae4884f4631ee4941d556d
clementdunstan <clementds@greentelecom.co.tz>
2026-08-28 17:01:29 +0300

    adding dataclass on the moe_v5 file

--- moe_v5.py
+++ moe_v5.py
@@ -6,6 +6,16 @@ import torch.nn.functional as F
 from dataclasses import dataclass
 
 
+@dataclass
+class MoEConfig :
+    hidden_state:int = 16
+    intermediate_state:int = 32
+    num_experts:int = 4
+
+    capacity_factor:float = 1.25
+    router_aux_loss_coef:float = 0.01
+
+
 class SwiguExpertModel(nn.Module):
     def __init__(self,hidden_state,intermediate_state):
         super().__init__()
@@ -24,6 +34,17 @@ class RoutingMetadata:
     expert_indices:torch.tensor
     routing_probs:torch.tensor
 
+    accepted_mask:torch.tensor
+    overflow_mask:torch.tensor
+
+    sorted_indices:torch.tensor
+
+    sorted_expert_ids:torch.tensor
+
+    unique_experts:torch.tensor
+    expert_counts:torch.tensor
+
+    original_positions:torch.tensor
 
 
 class MoeLayer(nn.Module):
