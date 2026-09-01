0e3a4dc771ac7f695948804d6f93891b65bf157e
clementdunstan <clementds@greentelecom.co.tz>
2026-09-01 16:53:00 +0300

    start implementing route tokens which is used to route token to the experts

--- moe_v5.py
+++ moe_v5.py
@@ -6,6 +6,7 @@ import torch.nn.functional as F
 from dataclasses import dataclass
 
 
+#CONFIGURATION CLASS 
 @dataclass
 class MoEConfig :
     hidden_state:int = 16
@@ -14,6 +15,7 @@ class MoEConfig :
 
     capacity_factor:float = 1.25
     router_aux_loss_coef:float = 0.01
+
 
 
 class SwiguExpertModel(nn.Module):
@@ -48,5 +50,60 @@ class RoutingMetadata:
 
 
 class MoeLayer(nn.Module):
-    def __init__(self):
+    def __init__(self,config:MoEConfig):
         super().__init__()
+
+        self.hidden_state = config.hidden_state
+        self.intermediate_state = config.intermediate_state
+        self.num_experts  = config.num_experts
+        self.capacity_factor = config.capacity_factor
+        self.router_aux_loss_coef = config.router_aux_loss_coef
+
+        self.router = nn.Linear(self.hidden_state,self.num_experts,bias=False)
+
+        self.experts = nn.ModuleList([
+            SwiguExpertModel(self.hidden_state,self.intermediate_state)
+            for _ in range(self.num_experts)
+        ])
+
+    #It is used to route token to the experts, so it decide which expert take which token
+    def route_tokens(self,tokens:torch.tensor):
+
+        # Get logits, logits means which has more likely to be selected compared to other but still it not in probability so it needs softmax
+        router_logits = self.router(tokens)
+
+        #Softmax the logits to get the probability, take probability across tokens (dim=-1)
+        router_probs = F.softmax(router_logits,dim=-1)
+
+        #From the router_probs across the tokens get the max probability and experts id for the expert for each token with top 1 
+        top1_probs,top1_experts = torch.max(router_probs,dim=-1)
+
+        #Get each experts probability on each expert column
+        probability_fraction = router_probs.mean(dim=0)
+
+        #Find routing fraction, token count
+        token_count = torch.bincount(router_probs,minlength=self.num_experts)
+
+        routing_fraction  = (token_count.float()/tokens.size(0))
+
+        #Find the auxially loss which is done during the training for balance token on each experts 
+        aux_loss = self.num_experts * torch.sum(probability_fraction * routing_fraction)
+
+        aux_loss = self.router_aux_loss_coef * aux_loss
+
+        return (
+            top1_probs,
+            top1_experts,
+            aux_loss
+        )
+
+
+
+
+
+
+
+
+
+
+
