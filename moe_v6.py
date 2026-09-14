92a04740d54cda082895eae7215b1e5992830c78
clementdunstan <clementds@greentelecom.co.tz>
2026-09-14 16:31:57 +0300

    Start implementation of top 2for the moe v6

--- moe_v6.py
+++ moe_v6.py
@@ -28,25 +28,61 @@ class SwiguExpertModel(nn.Module):
 @dataclass
 class RouteMetadata:
 
+    #This section will be used during the summation to get output. Eg context * probability of E1 + context * probabilty * E2
+    #This produce Top 2 expert indices. Eg [[0,1],[2,4]] 
     top2_experts_indices:torch.tensor
+    #This produce Top 2 expert probability. Eg [[0.6,0.7],[0.7,0.8]]
     top2_routing_probs:torch.tensor
 
+    #This is used to simplify the access of the contents
+    #This is flatten experts id, as expected experts ids is [[E1,E2]] => [E1,E2]
     flat_expert_ids:torch.tensor
+    #This is used to flattening the tokens indices 
     flat_token_indices:torch.tensor
+    #This is used to flattening the routing probs 
     flat_probs:torch.tensor
 
-
+    #This return sorted items as it arrange all in order 
     sorted_expert_ids:torch.tensor
     sort_token_indices:torch.tensor
     sorted_probs:torch.tensor
 
+    #This return accepted tokens and overflow tokens from experts 
     accepted_mask:torch.tensor
     overflow_mask:torch.tensor
 
+    #As experts are unique so you need to get those unique expert and their respectively count 
     experts_count:torch.tensor
     experts_offset:torch.tensor
 
+    #This used to return to their original position (re-order)
     original_position:torch.tensor
 
     capacity:int
 
+
+class MoeLayer(nn.Module):
+
+    def __init__(self,config:MoeConfig):
+        super().__init__()
+        self.hidden_state = config.hidden_state
+        self.intermediate_state = config.intermediate_size
+        self.top_k = config.top_k
+        self.num_experts = config.num_experts
+        self.router_aux_coeffient = config.router_aux_coeffient
+        self.capacity_factor = config.capacity_factor
+
+        if self.top_k != 2:
+            raise ValueError("Moe v6 works for top k = 2")
+
+        if self.top_k > self.num_experts:
+            raise ValueError("The top k should not be greater than number of experts")
+
+        self.router = nn.Linear(self.hidden_state,self.num_experts,bias=False)
+
+        self.experts = nn.ModuleList(
+            [
+                SwiguExpertModel(self.hidden_state,self.intermediate_state)
+                for _ in range (self.num_experts)
+            ]
+        )
